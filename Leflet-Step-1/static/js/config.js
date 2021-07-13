59fbd092c526b2960dc94b33350630603ff04694
lmodenbach <lmodenbach@gmail.com>
2021-07-13 12:22:11 -0700

    draft tile layer, markers and popups

--- Leflet-Step-1/static/js/config.js
+++ Leflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoibG1vZGVuYmFjaCIsImEiOiJja3IyOG5lcW8yOXAzMnZvODBqZDdwcGMzIn0.TC56DU65VYGMMuleVsgG9g";
+const API_KEY = "pk.eyJ1IjoiY2FybGFpciIsImEiOiJja3FsZHc0NzMwMnF6MnNwcXc3OGhsYmZxIn0.VDTDjCZwEqpx8BPeL9470Q";
--- Leflet-Step-1/static/js/logic.js
+++ Leflet-Step-1/static/js/logic.js
@@ -3,6 +3,10 @@ var allLastSevenDaysURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/sum
 d3.json(allLastSevenDaysURL).then(function(response) {
 
     console.log(response);
+    
+function markerSize(quakeSize) {
+    return quakeSize;
+}
 
 var myMap = L.map("quakeMap", {
     center: [37.8044, 122.2712],
@@ -18,6 +22,17 @@ var myMap = L.map("quakeMap", {
     accessToken: API_KEY
   }).addTo(myMap);
 
+// Loop through the earthquakes array and create one marker for each earthquake object
+for (var i = 0; i < response.length; i++) {
+    L.circle(([response.features[index].geometry.coordinates[1], response.features[index].geometry.coordinates[0]]), {
+      fillOpacity: 0.75,
+      color: "rgb(215, 154, 227)",
+      fillColor: "rgb(247, 171, 47)",
+      // Setting our circle's radius equal to the output of our markerSize function
+      // This will make our marker's size proportionate to its quake magnitude
+      radius: markerSize(response.features[i].properties.mag)
+    }).bindPopup("<h1>" + response.features[i].properties.mag + "</h1> <hr> <h3>Magnitude Earthquake</h3>").addTo(myMap);
+}
 });
 
 
