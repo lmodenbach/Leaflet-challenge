54baf90d2c2aa37099e773425625feb9448601a7
lmodenbach <lmodenbach@gmail.com>
2021-07-19 20:50:39 -0700

    Got Step2!!! Cleanup time

--- Leaflet-Step2/static/js/logic.js
+++ Leaflet-Step2/static/js/logic.js
@@ -1,16 +1,3 @@
-var myMap = L.map("quakeMap", {
-    center: [37.8044363, -122.271111],
-    zoom: 5
-  });
-  
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 500,
-    maxZoom: 20,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  }).addTo(myMap);
 
 var allLastSevenDaysURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
@@ -49,31 +36,26 @@ function circleColor(quakeDepth) {
   return color;
 }
 
+var earthquakes = new L.LayerGroup();
 
 for (var i = 0; i < response.features.length; i++) {
-    L.circle(([response.features[i].geometry.coordinates[1], response.features[i].geometry.coordinates[0]]), {
+      L.circle(([response.features[i].geometry.coordinates[1], response.features[i].geometry.coordinates[0]]), {
       fillOpacity: 0.75,
       color: "black",
       weight: 0.5,
       fillColor: circleColor(Math.abs(response.features[i].geometry.coordinates[2])),
       radius: markerSize(Math.abs(response.features[i].properties.mag))
-      
-    }).bindPopup("<h3>Magnitude: " + response.features[i].properties.mag + 
+      }).bindPopup("<h3>Magnitude: " + response.features[i].properties.mag + 
                  "</h3><hr><h3>Depth: " + Math.abs(response.features[i].geometry.coordinates[2]) + "</h3><hr>" +
                  "<h3>Lat: " + response.features[i].geometry.coordinates[1] + ", Lng: " + 
-                 response.features[i].geometry.coordinates[0] + "</h3>")
-    .addTo(myMap);
-    
-  } 
+                 response.features[i].geometry.coordinates[0] + "</h3>").addTo(earthquakes);
+} 
 
-
-// Create a legend to display information about our map
-var info = L.control({
+var legend = L.control({
   position: "bottomright"
 });
 
-// When the layer control is added, insert a div with the class of "legend"
-info.onAdd = function() {
+legend.onAdd = function() {
   var div = L.DomUtil.create("div", "legend");
   div.innerHTML = "<h4>Earthquake Depth (KM)</h4>" + [
     "<div class=\"box\" style=\"background-color: rgb(58, 240, 64)\"> < 5 </div>", 
@@ -88,11 +70,6 @@ info.onAdd = function() {
   ].join("<br>");
   return div;
 };
-// Add the info legend to the map
-info.addTo(myMap);
-
-
-});
 
 
 d3.json("./static/data/tectonicplates-master/GeoJSON/PB2002_boundaries.json").then(function(geoData) {
@@ -105,5 +82,57 @@ d3.json("./static/data/tectonicplates-master/GeoJSON/PB2002_boundaries.json").th
     var plates = L.geoJSON(geoData, {
       onEachFeature: onEachFeature
     });
+
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 500,
+      maxZoom: 20,
+      zoomOffset: -1,
+      id: "mapbox/streets-v11",
+      accessToken: API_KEY
+    });
+
+    var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/satellite-v9",
+      accessToken: API_KEY
+    });;
+
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/light-v10",
+      accessToken: API_KEY
+    });;
+
+  var baseMaps = {
+      "Street Map": streetmap,
+      "Satellite Map": satellitemap,
+      "Light Map": lightmap
+  };
+
+  var overlayMaps = {
+    Plates: plates,
+    Earthquakes: earthquakes
+  };
+
+  var myMap = L.map("quakeMap", {
+    center: [37.8044363, -122.271111],
+    zoom: 5,
+    layers: [satellitemap, plates, earthquakes]
+  });
+
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+  legend.addTo(myMap);
+
+});
 
 });
